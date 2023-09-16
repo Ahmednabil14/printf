964a6639708df3dfe6a69f9595a810df454ef394
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-16 14:31:39 -0700

    1

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef _PRINTF_
+#define _PRINTF_
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
