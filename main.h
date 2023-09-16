561df2c136f7058873676555a4f550894352834d
Ahmednabil14 <ahmednabil14499@gmail.com>
2023-09-16 15:50:08 -0700

    print_str

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+int printf_str(va_list list);
 int _printf(const char *format, ...);
 void printf_char(va_list list);
 int _putchar(char c);
